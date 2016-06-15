80ac75068f6931c14d91b0d307464d79e1789f22
Tanapruk <trusthaha@gmail.com>
2016-06-15 23:45:04 +0700

    add comment for better understanding

--- App.js
+++ App.js
@@ -3,18 +3,25 @@ import { Component } from 'react';
 import Widget from './Widget';
 import Header from './Header';
 
+//export default = allow other classes to import like above
 export default class App extends Component {
+	//render() is a required override method for component.
+	
 	render() {
-		const data = 'asdfasdf';
-		console.log('render()');
+		const data = 'This is data for widget object.';
+		console.log('render()App')
+		//render can only return one dom object, so we have to pack it inside <div>
 		return (
 			<div>
 			<h1> Hello Nextzy</h1>
-			<h2>ssss</h2>
+			<h2>This is header2</h2>
 			<Widget name={data}/>
 			<Header/>
 			</div>
 		)
+		//the Widget is an object which takes a parameter call name.
+		//Header is another imported object. You have to look inside Header.js to see what it consists of.
+		//in javascript, we use bracket when referring to a variable.
 	}
 
 
