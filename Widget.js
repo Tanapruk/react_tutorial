f2eba41e9f66c4dd08c19ae8479fd16f8fcecf6e
Tanapruk <trusthaha@gmail.com>
2016-06-15 16:54:25 +0700

    init file

---
+++ Widget.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Component } from 'react';
+
+
+
+export default class Widget extends Component {
+	render() {
+		return (
+		<div>
+			<h1> Widget </h1>
+			<p>{this.props.name}</p>
+		</div>
+		)
+	}
+}
+
+export default Widget
+
+
+
