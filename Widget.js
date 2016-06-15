575fcbf1050478d5f58b3f0d2c1dc3b5009f7400
Tanapruk <trusthaha@gmail.com>
2016-06-15 23:57:40 +0700

    update comment for better understanding

--- Widget.js
+++ Widget.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Component } from 'react';
-
-
+import {Component} from 'react';
 
 export default class Widget extends Component {
+	console.log('render()Widget')
+	//{this.props.name} --> create property variable called name and put it to the <p> tag.
+	//As you  see on the App.js, we can pass an argument with a name="xx" to the <p> tag.
 	render() {
 		return (
 		<div>
