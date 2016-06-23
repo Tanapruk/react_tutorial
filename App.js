dd1c260b43ea4e175af6663aaa0e550923444b70
tanapruk <trusthaha@gmail.com>
2016-06-24 00:28:50 +0700

    pretty text

--- App.js
+++ App.js
@@ -5,25 +5,22 @@ import Header from './Header';
 
 //export default = allow other classes to import like above
 export default class App extends Component {
-	//render() is a required override method for component.
-	
-	render() {
-		const data = 'This is data for widget object.';
-		console.log('render()App')
-		//render can only return one dom object, so we have to pack it inside <div>
-		return (
-			<div>
-			<h1> Hello Nextzy</h1>
-			<h2>This is header2</h2>
-			<Widget name={data}/>
-			<Header/>
-			</div>
-		)
-		//the Widget is an object which takes a parameter call name.
-		//Header is another imported object. You have to look inside Header.js to see what it consists of.
-		//in javascript, we use bracket when referring to a variable.
-	}
+    //render() is a required override method for component.
+
+    render() {
+        const data = 'This is data for widget object.';
+        console.log('render()App')
+            //render can only return one dom object, so we have to pack it inside <div>
+        return ( < div >
+                < h1 > Hello Nextzy < /h1> < h2 > This is header2 < /h
+                2 > < Widget name = { data }
+                /> < Header / >
+                < /div>
+            )
+            //the Widget is an object which takes a parameter call name.
+            //Header is another imported object. You have to look inside Header.js to see what it consists of.
+            //in javascript, we use bracket when referring to a variable.
+    }
 
 
 }
-
--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 
 //import the render function of react-dom
 //use it to build App.js into the element of Html called 'app'
 
 const node = document.getElementById('app')
-render(<App/>, node)
-
-
+render( < App / > , node)
